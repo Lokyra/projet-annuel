5eb29d97e05facbd5fb727e9e4e013ac474518f6
ayoub <ayoubousbia02@gmail.com>
2024-03-21 18:23:31 +0100

    update app.php displaying dynamically user pseudo if logged in & ability to signout

--- playerOnline/app.php
+++ playerOnline/app.php
@@ -1,11 +1,116 @@
+<?php
+session_start(); 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="shortcut icon" type="image/png" href="asset/favicon.png"/>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/figure.css">
+    <link rel="stylesheet" href="css/play.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&family=Rubik+Mono+One&display=swap" rel="stylesheet">
+    <title>Tic-Tac-Toe</title>
 </head>
 <body>
-    <h1>You are logged In !</h1>
+    <header class="container">
+        <div>
+            <img src="asset/logo.png" height="20px">
+            <span class="logo-text">Tic-tac-toe.com</span>
+        </div>
+        <nav class="navigation">
+            <div class="navigation-item">Play</div>
+            <div class="navigation-item">Puzzles</div>
+            <div class="navigation-item">Learn</div>
+            <div class="navigation-item">Watch</div>
+        </nav>
+        <div class="action-buttons">
+        <?php
+            if(isset($_SESSION['pseudo'])){ 
+                echo '<a href="profile.php" class="login-btn">' . $_SESSION['pseudo'] . '</a>';
+            } else {
+                echo '<a href="login.php" class="login-btn">Login</a>';
+            }
+            
+            if(isset($_SESSION['pseudo'])){ 
+                echo '<a href="signout.php" class="sign-up-btn"> Deconnexion</a>';
+            } else {
+                echo '<a href="signup.php" class="sign-up-btn">Sign Up</a>';
+            }
+            ?>
+       
+        </div>
+    </header>
+    <main>
+        <div class="figure">
+        <section class="section-padding">
+            <div class="flex-row">
+              <div class="flex-column wide-column">
+                <div class="square"></div>
+              </div>
+
+              <div class="flex-column narrow-column">
+                <div class="square-middle">
+                  <img src="asset/cross.png"  class="img">
+                </div>
+                
+              </div>
+              <div class="flex-column wide-column">
+                <div class="image-content-last"></div>
+              </div>
+              
+            </div>
+          </section>
+
+          
+          <section class="section-padding">
+            <div class="flex-row">
+              <div class="flex-column wide-column">
+                <div class="square">
+                  <div class="circle"></div>
+                </div>
+              </div>
+              <div class="flex-column narrow-column">
+                <div class="square-middle">
+                  <img src="asset/cross.png"  class="img">
+                </div>
+              </div>
+              <div class="flex-column wide-column">
+                <div class="image-content-last"></div>
+              </div>
+              
+            </div>
+          </section>
+          <section class="section-padding-last">
+            <div class="flex-row">
+              <div class="flex-column wide-column">
+                <div class="image-content"></div>
+              </div>
+              <div class="flex-column narrow-column">
+                <div class="square-last">
+                  <div class="circle"></div>
+                </div>
+              </div>
+              <div class="flex-column wide-column">
+                <div class="image-content-last"></div>
+              </div>
+              
+            </div>
+          </section>
+        </div>
+
+        <section class="play-options-container">
+            <a href="play.html" class="play-online">Play Online</a>
+            <a href="play-computer.html" class="play-computer">Play Against Computer</a>
+        </section>
+
+        <section>
+          <h1 class="game-title">Play <br>tic-tac-toe<br> online !</h1>
+        </section>
+        
+    </main>
 </body>
 </html>
