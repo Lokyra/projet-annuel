6f2cfc539e4dcc7c9cc866da32b541acf0177f01
AntoineDeparis <158306939+AntoineDeparis@users.noreply.github.com>
2024-03-23 22:58:38 +0100

    Update profile.php

--- playerOnline/profile.php
+++ playerOnline/profile.php
@@ -2,10 +2,63 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="asset/favicon.png"/>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/figure.css">
+    <link rel="stylesheet" href="css/play.css">
+    <link rel="stylesheet" href="../adminOnline/css/backoffice.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&family=Rubik+Mono+One&display=swap" rel="stylesheet">
     <title>Tic-Tac-Toe</title>
 </head>
 <body>
-    <h1> Votre Profil sera afficher ici </h1>
+    <header class="container">
+        <div>
+            <img src="asset/logo.png" height="20px">
+            <span class="logo-text">Tic-tac-toe.com</span>
+        </div>
+        <nav class="navigation">
+            <div class="navigation-item">Play</div>
+            <div class="navigation-item">Puzzles</div>
+            <div class="navigation-item">Learn</div>
+            <div class="navigation-item">Watch</div>
+        </nav>
+        <div class="action-buttons">
+            <a href="login.php"class="login-btn">Login</a>
+            <a href="signup.php"class="sign-up-btn">Sign Up</a>
+        </div>
+    </header>
+    <main>
+        <div class="stats-container">
+            <div class="stats-box">Dernière connnection : 1 day</div>
+            <div class="stats-box">Nombre de parties jouer: 128 000</div>
+            <div class="stats-box">Nombre de partie gagné: 1024 000</div>
+            <div class="stats-box">Nombre de partie perdu: 1024 000</div>
+        </div>
+        <div class="table-wrapper">
+            <table class="game-table">
+                <tr>
+                    <th>Name of Account</th>
+                    <th>Stats</th>
+                    <th>Email Address</th>
+                    <th>Password</th>
+                    <th>Creation of the account</th>
+                    <th>Guild</th>
+                </tr>
+                <tr>
+                    <td class="table-cell">Player 1</td>
+                    <td class="table-cell">50% Win, 50% lose</td>
+                    <td class="table-cell">player1@example.com</td>
+                    <td class="table-cell">********</td>
+                    <td class="table-cell">2024-02-25 08:00 AM</td>
+                    <td class="table-cell">Guild A</td>
+                </tr>
+            </table>
+        </div>
+    </main>
+    </main>
 </body>
 </html>
